9271b30c026c2fe5d3e632146e729c91874e6ba1
Harvey Zhao <86583647+harveyzzhao@users.noreply.github.com>
2022-11-06 23:40:25 -0800

    built signup popup skeleton

--- frontend/src/components/signupPopup.js
+++ frontend/src/components/signupPopup.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function SignupPopup() {
     return(
         <div className="popup">
-            <h1>Get Started</h1>
+            <h2>Get Started</h2>
             <div className="formcontainer">
                 <div className="name">
-                    <input className="namefield" type="text" placeholder="First Name:" name="firstname" required></input>
-                    <input className="namefield" type="text" placeholder="Last Name:" name="lastname" required ></input>
+                    <input className="nameform" type="text" placeholder="First Name:" name="firstname" required></input>
+                    <input className="nameform" type="text" placeholder="Last Name:" name="lastname" required ></input>
                 </div>
                 <input className="popupform" type="password" placeholder="Set a password: " name="pwd" required></input>
                 <button type="submit">Join</button>
--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -14,7 +14,7 @@ export default function LandingPage() {
             <Slogan/>
             <Signup/>
             {/* <LoginPopup/> */}
-            {/* <SignupPopup/> */}
+            <SignupPopup/>
         </body>
     );
 }
