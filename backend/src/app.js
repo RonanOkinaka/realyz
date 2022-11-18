0e81394ecda4843516fd5bdc5c64709ccc40f3f2
Harvey Zhao <86583647+harveyzzhao@users.noreply.github.com>
2022-11-17 19:54:26 -0800

    Added CORS to backend and added a test function to test response from backend server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,35 @@
-import express from 'express';
+import express, { application } from 'express';
+import cors from 'cors';
 
 // Workaround to enforce early configuration
 import './config.js';
 import sessionRoute from './route/session.js';
 import userRoute from './route/user.js';
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionsSuccessStatus: 200,
+}
+
+const PORT = process.env.PORT || 8080;  //localhost port
 // Set up Express.js routing
 // TODO: some CORS and HTTPS configuration may be necessary here
 const server = express();
+server.use(cors(corsOptions));
 server.use(express.json());
 server.use('/session', sessionRoute);
 server.use('/user', userRoute);
 
-server.listen(process.env.PORT, function(err, _data) {
+// server.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server!" });
+//   });
+
+// server.get("/", (req, res) => {
+//     res.json({ message: "ddd from server!" });
+// });
+
+server.listen(PORT, function(err, _data) {
     if (err) {
         console.error(err);
     } else {
