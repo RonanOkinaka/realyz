4fd3ef32d078afea17e2a6ce6059d00c47f2dfd5
Ronan Okinaka <48971205+RonanOkinaka@users.noreply.github.com>
2022-11-29 23:15:56 -0800

    Add username search (#20)
    * Add search functionality
    
    Added ability to query for users by uid, email, first/last name,
    type and company.
    
    For string data, it is possible to search for matches that START
    with the query. Substring searches are not currently supported for
    performance reasons (full table scan!). Also overhauled column
    verification for the users table.
    
    * Add connection searches to /search
    
    Added the ability to filter /search results to only connected
    users. Requires authorization. The search model started getting
    rather large, so I moved it into its own source file, as well.
    
    * Add random user search

---
+++ backend/src/controller/search.js
@@ -0,0 +1,32 @@
+import * as searchModel from '../model/search.js';
+
+export async function searchUsers(req, res) {
+    try {
+        const { wildcard, connected } = req.query;
+        delete req.query.wildcard;
+        delete req.query.connected;
+
+        const { uid } = req.body;
+
+        const [err, results] = await searchModel.searchUsers(
+            req.query, wildcard, connected, uid
+        );
+        if (err !== null) {
+            return res.status(err.code).json({ error: err.message });
+        }
+
+        return res.status(200).json(results);
+    } catch (except) {
+        console.error(except);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+export async function getRandomUsers(_req, res) {
+    try {
+        return res.status(200).json(await searchModel.getRandomUsers());
+    } catch (except) {
+        console.error(except);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
