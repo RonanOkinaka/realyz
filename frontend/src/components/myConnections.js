78e17cd4fd79bffc94e533e666f98720c005b5e3
Abhay Rao <abhayrao25@gmail.com>
2022-11-29 22:41:30 -0800

    Better organized the connections code

--- frontend/src/components/myConnections.js
+++ frontend/src/components/myConnections.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import SamplePic from "../media/sample.jpg";
 
+const Connection = ({connectName}) => {
+    return (
+        <div class="item">
+            <img src={SamplePic} />
+            <span class="caption">{connectName}</span>
+        </div>
+    )
+}
 const MyConnections = ({ vis }) => {
     return ((vis == 1) ? (
         <div className="myprofile">
@@ -10,75 +18,7 @@ const MyConnections = ({ vis }) => {
                 <input type="submit" name="submit" class="submit" value="Search" />
             </form>
             <div class="connectionGallery">
-                <div class="item">
-                    <img src={SamplePic} />
-                    <span class="caption">John Doe</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?12" />
-                    <span class="caption">Ronan</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?13" />
-                    <span class="caption">Abhay</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?14" />
-                    <span class="caption">David</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                <div class="item">
-                    <img src="https://picsum.photos/190/190?15" />
-                    <span class="caption">Raj</span>
-                </div>
-                
+                <Connection connectName={"Abhay Rao"} />
             </div>
         </div>
     ) : null);
