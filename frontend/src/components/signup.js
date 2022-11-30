242b45899b0b15104ca9cedfe46df5ce3684afba
Raj <rukrajagrawal@Rajs-MacBook.local>
2022-11-29 22:27:25 -0800

    CHECKED EMAIL STRING FOR @ SYMBOL

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -12,14 +12,23 @@ export default function Signup({show}) {
         let username = email;
         event.preventDefault(); // prevent page refresh
         // prevent popup if user does NOT fill in a valid email.
+        let hasAt = false;
+        for (let i = 0; i < email.length; i++)
+                if (email[i] == '@')
+                    hasAt = true;
         if (email.length === 0) {
             setErr("your email isn't valid.");
             setStatus(false);
         }
-        else{
+        else if (hasAt == true) {
+            
             storeUserData(['uid', 'email'], [username, email]);
             setErr(null);
             setStatus(true);
+        }
+        else 
+        {
+            setErr("your email isn't valid.");
         }
     };
 
