fb3df037287888698bb9fee4368c51e5ea3d0b78
Abhay Rao <abhayrao25@gmail.com>
2022-11-29 20:34:55 -0800

    Added heading to the Sent Requests subpart.

--- frontend/src/components/sentRequests.js
+++ frontend/src/components/sentRequests.js
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react";
 const SentRequests = ({vis}) => {
     return (vis==1) ?(
         <div className="requestsContainer">
-            <p>Sent Requests</p>
+            <div classname="requestsSubheading"><p>Sent Requests</p></div>
             <div className="request">
                 <p>John Doe</p>
             </div>
