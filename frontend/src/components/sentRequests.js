5e4faa99af2998e7ed36e8916600a7de48860c85
Abhay Rao <abhayrao25@gmail.com>
2022-11-30 23:21:55 -0800

    Display my connections is now dynamic.

--- frontend/src/components/sentRequests.js
+++ frontend/src/components/sentRequests.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { getLocalUserData, getConnections, getUserData } from "../util/data";
 
 const SentRequest = ({requestee}) => {
     return (
@@ -10,13 +11,27 @@ const SentRequest = ({requestee}) => {
 }
 
 const SentRequests = ({vis}) => {
+    const [connections, setConnections] = useState([]);
+    let query = {
+        "pending" : 1,
+        "from": getLocalUserData(['uid'])['uid'],
+    }
+    useEffect(() => {
+    getConnections(query)
+    .then(function(res){
+        //console.log(res.data.connections);
+        setConnections(res.data.connections);
+    });}, [])
+    /*for (let i = 0; i < res.data.connections.length; i++){
+      
+    }*/
+    
     return (vis==1) ?(
         <div className="requestsContainer">
             <p>Sent Requests</p>
-            <SentRequest requestee={"Abhay"} />
-            <SentRequest requestee={"Raj"} />
-            <SentRequest requestee={"Ronan"} />
-            <SentRequest requestee={"David"} />
+            {connections.map(obj => (
+                <SentRequest requestee={obj.uidTo} />
+            ))}
         </div>
     ) : null;
 }
