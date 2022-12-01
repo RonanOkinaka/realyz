c0d0ff7732d8d4d58f30d5e80cfcf7e670979f20
davaron <djiaronglin@gmail.com>
2022-11-30 21:18:24 -0800

    fixed css

--- frontend/src/util/data.js
+++ frontend/src/util/data.js
@@ -141,7 +141,7 @@ const createConnection = (to) => axios ({
     }
 });
 
-//pending: 0 = pending, 1 = established
+//pending: 1 = pending, 0 = established
 const getConnections = (query) => axios ({
     method: 'get',
     baseURL: 'http://localhost:8080',
