4d5d08f8870230d676ab7541464beffd60810629
Abhay Rao <abhayrao25@gmail.com>
2022-12-01 00:51:26 -0800

    For sent requests, the box now dynamically displays what connections the user sent.

--- frontend/src/components/sentRequests.js
+++ frontend/src/components/sentRequests.js
@@ -12,6 +12,7 @@ const SentRequest = ({requestee}) => {
 
 const SentRequests = ({vis}) => {
     const [connections, setConnections] = useState([]);
+    const names = [];
     let query = {
         "pending" : 1,
         "from": getLocalUserData(['uid'])['uid'],
@@ -19,18 +20,21 @@ const SentRequests = ({vis}) => {
     useEffect(() => {
     getConnections(query)
     .then(function(res){
-        //console.log(res.data.connections);
-        setConnections(res.data.connections);
+        for (let i = 0; i < 5; i++){
+            getUserData(res.data.connections[i]['uidTo']).then(function(r2) {
+                console.log("this is exec");
+                names.push(r2.data.fname +" " + r2.data.lname);
+                setConnections(names);
+            });
+          }
     });}, [])
-    /*for (let i = 0; i < res.data.connections.length; i++){
-      
-    }*/
-    
+    //setConnections()
+    console.log(connections);
     return (vis==1) ?(
         <div className="requestsContainer">
             <p>Sent Requests</p>
             {connections.map(obj => (
-                <SentRequest requestee={obj.uidTo} />
+                <SentRequest requestee={obj} />
             ))}
         </div>
     ) : null;
