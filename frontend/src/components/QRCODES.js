a6275d4c5d260cf2e1453d917674579189685be3
Raj <rukrajagrawal@Rajs-MacBook.local>
2022-11-30 20:26:19 -0800

    Added documentation to QRcodes object

--- frontend/src/components/QRCODES.js
+++ frontend/src/components/QRCODES.js
@@ -2,16 +2,16 @@ import QRCode from 'qrcode';
  import { useEffect, useState } from 'react';
 
 
- const QRobject = ({ text }) => {
+ const QRobject = ({ text }) => { // object for qrcodes
      const [src, setSrc] = useState("");
 
      useEffect(() => {
-         QRCode.toDataURL(text).then((setSrc));
+         QRCode.toDataURL(text).then((setSrc)); // sets data for qrcodes
      }, []);
 
      return (
          <div>
-           <img src ={src} />
+           <img src ={src} /> 
          </div>
      );
  };
