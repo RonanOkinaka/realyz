b39dd069033eb1235a1a0e4783126ff68ee7df91
Harvey Zhao <86583647+harveyzzhao@users.noreply.github.com>
2022-12-02 01:53:25 -0800

    fixed compile errors as a result of merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Profile from './pages/profile';
 import MainPage from './pages/main';
 import NoPage from './pages/noPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/main';
 import SearchResultPage from './pages/searchResult';
 import testPage from './pages/test';
 import FullOtherProfile from './pages/otherProfile';
